6929e647404250e289c2dc0888c24f65bc5811ae
Brandon Riden <brandon.riden@onyxpoint.com>
2018-03-08 14:25:38 -0500

    fixing release command

--- lib/simp/metadata/commands/release.rb
+++ lib/simp/metadata/commands/release.rb
@@ -12,14 +12,13 @@ module Simp
 
           engine, root = get_engine(engine, options)
           begin
-            release = argv[0]
-            section = argv[1]
+            section = argv[0]
             case section
               when "components"
-                puts engine.releases[release].components.keys.join("\n")
+                puts engine.releases[options["release"]].components.keys.join("\n")
               else
                 puts "components:"
-                engine.releases[release].components.keys.each do |line|
+                engine.releases[options["release"]].components.keys.each do |line|
                   puts "    #{line}"
                 end
             end
